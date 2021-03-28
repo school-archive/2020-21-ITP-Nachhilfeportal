0d60304823369d8d99dcc751417ce55e279e2880
stummer <matt.stummer@outlook.de>
2021-03-28 21:32:50 +0200

    Updated Tutor.php

--- backend/protected/classes/Tutor.php
+++ backend/protected/classes/Tutor.php
@@ -1,6 +1,7 @@
 <?php
+require_once __DIR__ . '/../mysql_manager.php';
 
-abstract class Tutor extends User
+class Tutor extends Student
 {
     private $description;
     private $teaching_method;
@@ -16,15 +17,73 @@ abstract class Tutor extends User
         $this->teaching_method = $teaching_method;
     }
 
+    /**
+     * @return mixed $description
+     */
+
     public function getDescription()
     {
         return $this->description;
     }
 
+    /**
+     * @param $description
+     */
+
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return mixed $teaching_method
+     */
 
     public function getTeaching_method()
     {
         return $this->teaching_method;
     }
 
+    /**
+     * @param $teaching_method
+     */
+
+    public function setTeaching_method($teaching_method)
+    {
+        $this->teaching_method = $teaching_method;
+    }
+
+    /**
+     * @param $email
+     * @return Tutor
+     */
+
+    static function get_tutor_by_email($email)
+    {
+        $s = get_np_mysql_object()->prepare("select * from tutor where email = :email");
+        $s->execute(array(":email" => $email));
+        $obj = $s->fetch();
+        $tutor = new tutor($email, $obj['description'], $obj['teaching_method']);
+        $tutor->setDescription($obj['description']);
+        $tutor->setTeaching_method($obj['teaching_method']);
+        return $tutor;
+    }
+
+    /**
+     * @param $email
+     * @param $description
+     * @param $teaching_method
+     * @return Tutor
+     */
+    static function create_tutor($email, $description, $teaching_method)
+    {
+        $s = get_np_mysql_object()->
+        prepare("insert into tutor (desciption, teaching_method) 
+        values (:description, :teaching_method");
+        $s->execute(array(
+            ":description" => $description,
+            ":teaching_method" => $teaching_method
+        ));
+        return new Tutor($email, $description, $teaching_method);
+    }
 }
