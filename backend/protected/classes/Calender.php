10f869a1f3bfa53cd096fa80ce0ab269e5bd050c
stummer <matt.stummer@outlook.de>
2021-04-14 11:07:54 +0200

    Created Calender.php functions

--- backend/protected/classes/Calender.php
+++ backend/protected/classes/Calender.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace classes;
+
 use JsonSerializable;
 use PDO;
 
@@ -8,7 +10,46 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 class Calender
 {
+    private $from;
+    private $to;
+    private $weekday;
 
+    /**
+     * Tutor constructor.
+     * @param $email
+     * @param $first_name
+     * @param $last_name
+     * @param $password
+     * @param $picture_url
+     * @param $from
+     * @param $to
+     * @param $weekday
+     * @param int $types
+     * @param false $locked
+     */
 
+    public function __construct($email, $first_name, $last_name, $password, $picture_url, $from, $to, $weekday, $grade, $department, $types = 0, $locked = false)
+    {
+        parent::__construct($email, $first_name, $last_name, $password, $picture_url, $grade, $department, $types, $locked);
+        $this->from = $from;
+        $this->to = $to;
+        $this->weekday = $weekday;
+
+    }
+
+    public function getTimeFROM()
+    {
+        return from;
+    }
+
+    public function getTimeTO()
+    {
+        return to;
+    }
+
+    public function getWeekday()
+    {
+        return weekday;
+    }
 
 }
