2dbcfc162bd7d43a27222a2db71dcc656645640c
hannik03 <7047@htl.rennweg.at>
2021-04-27 20:43:30 +0200

    add Calendar

---
+++ frontend/src/pages/profil/Profil.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+  <Calendar></Calendar>
+
+</div>
+</template>
+
+<script>
+import Calendar from "../../components/Calendar";
+export default {
+  name: "Profil",
+  components: {Calendar}
+}
+</script>
+
+<style scoped>
+
+</style>
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,4 +3,8 @@ module.exports = [
         path: '/',
         component: () => import('@/pages/home/Home')
     },
+    {
+        path: '/profil',
+        component: () => import('@/pages/profil/Profil')
+    },
 ];
