4f2a8b2a554b34d6cee5d9c9ec4d8df92ac0280e
kopsi <dietrich.kops@htl.rennweg.at>
2021-05-04 20:16:30 +0200

    added imprint

---
+++ frontend/src/pages/imprint/Imprint.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <img src="@/assets/images/Imprint.png" alt="imprint" height="300px" width="100%">
+    <div>
+      <p class="u1">Impressum</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Imprint"
+}
+</script>
+
+<style scoped>
+.u1{
+
+}
+
+
+</style>
