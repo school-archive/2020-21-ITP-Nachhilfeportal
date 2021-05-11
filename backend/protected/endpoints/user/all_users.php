3e680269743a96f55a109471bebd9d1f9b0ad77f
Nicoletta2003-1 <71252015+Nicoletta2003-1@users.noreply.github.com>
2021-05-11 13:53:57 +0200

    added if in endpoint

--- backend/protected/endpoints/user/all_users.php
+++ backend/protected/endpoints/user/all_users.php
@@ -8,18 +8,22 @@ use classes\AnswerHandler;
 if (! Authentication::is_logged_in())
     AnswerHandler::create_response_and_kill_page(false, "unauthorized", 401);
 
-$user = User::getUser(Authentication::$user_email);
+
+if($_SERVER['REQUEST_METHOD'] === 'GET') {
+    $user = User::getUser(Authentication::$user_email);
+
+    if(isset($_GET['method'])) {
+        if($_GET['method']==='filter') {
+            AnswerHandler::create_response_and_kill_page(true, $user->filterUser());
+        }
+    }
 
 
-if($_GET['method']==='filter') {
-    AnswerHandler::create_response_and_kill_page(true, $user->filterUser());
+    if (!$user->isAdmin())
+        AnswerHandler::create_response_and_kill_page(false, "admin privileges required", 403);
+
+    $count = isset($_GET["count"]) ? $_GET["count"] : 10;
+    $offset = isset($_GET["offset"]) ? $_GET["offset"] : 0;
+
+    AnswerHandler::create_response_and_kill_page(true, User::get_users($count, $offset));
 }
-
-
-if (!$user->isAdmin())
-    AnswerHandler::create_response_and_kill_page(false, "admin privileges required", 403);
-
-$count = isset($_GET["count"]) ? $_GET["count"] : 10;
-$offset = isset($_GET["offset"]) ? $_GET["offset"] : 0;
-
-AnswerHandler::create_response_and_kill_page(true, User::get_users($count, $offset));
