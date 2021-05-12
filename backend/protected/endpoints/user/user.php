5213c74116a4fb85c96ebd625ad2770a659685be
stummer <matt.stummer@outlook.de>
2021-05-12 10:26:26 +0200

    unsettutor endpoint

--- backend/protected/endpoints/user/user.php
+++ backend/protected/endpoints/user/user.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once __DIR__ . "/../../vendor/autoload.php";
+
 use classes\User;
 use classes\AnswerHandler;
 use classes\Authentication;
@@ -10,11 +11,10 @@ switch ($_SERVER['REQUEST_METHOD']) {
         $user = User::class;
 
         if ($_GET["email"] == "@me") {
-            if (! Authentication::is_logged_in())
+            if (!Authentication::is_logged_in())
                 AnswerHandler::create_response_and_kill_page(false, "unauthorized", 401);
             $user = User::getUser(Authentication::$user_email);
-        }
-        else {
+        } else {
             $user = User::getUser($_GET["email"]);
         }
 
@@ -24,13 +24,29 @@ switch ($_SERVER['REQUEST_METHOD']) {
 
         //matthias
 
+        if (isset($_GET['action'])) {
+            if (!Authentication::is_logged_in())
+                AnswerHandler::create_response_and_kill_page(false, "unauthorized", 401);
+            if ($_GET['action'] === 'ifunsettutor') {
+                $user = User::getUser(Authentication::$user_email);
+                if (!$user->isTutor()) {
+                    if ($user->getGrade() == null) {
+                        if ($user->getDepartment() == null) {
+                            AnswerHandler::create_response_and_kill_page(true, ["unsettutor" => true]);
+                        }
+                    }
+                } else {
+                    AnswerHandler::create_response_and_kill_page(true, ["unsettutor" => false]);
+                }
+            }
+        }
 
 
         //ich
         break;
 
     case "DELETE":
-        if (! Authentication::is_logged_in())
+        if (!Authentication::is_logged_in())
             AnswerHandler::create_response_and_kill_page(false, "unauthorized", 401);
 
         $user = User::getUser(Authentication::$user_email);
