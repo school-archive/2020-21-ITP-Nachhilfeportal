ea5ba1dae234337a3b635ff1c1c8e9651b618601
Nicoletta2003-1 <71252015+Nicoletta2003-1@users.noreply.github.com>
2021-05-12 10:02:53 +0200

    added profilinfo endpoint

--- backend/protected/endpoints/user/user.php
+++ backend/protected/endpoints/user/user.php
@@ -1,7 +1,6 @@
 <?php
 
 require_once __DIR__ . "/../../vendor/autoload.php";
-
 use classes\User;
 use classes\AnswerHandler;
 use classes\Authentication;
@@ -10,16 +9,19 @@ switch ($_SERVER['REQUEST_METHOD']) {
     case "GET":
         $user = User::class;
 
-        if ($_GET["email"] == "@me") {
-            if (!Authentication::is_logged_in())
-                AnswerHandler::create_response_and_kill_page(false, "unauthorized", 401);
-            $user = User::getUser(Authentication::$user_email);
-        } else {
-            $user = User::getUser($_GET["email"]);
-        }
+        if(isset($_GET["email"])) {
+            if ($_GET["email"] == "@me") {
+                if (!Authentication::is_logged_in())
+                    AnswerHandler::create_response_and_kill_page(false, "unauthorized", 401);
+                $user = User::getUser(Authentication::$user_email);
+            } else {
+                $user = User::getUser($_GET["email"]);
+            }
 
-        if (!$user) AnswerHandler::create_response_and_kill_page(false, "user not found", 404);
-        AnswerHandler::create_response_and_kill_page(true, $user);
+            if (!$user) AnswerHandler::create_response_and_kill_page(false, "user not found", 404);
+            AnswerHandler::create_response_and_kill_page(true, $user);
+
+        }
 
 
         //matthias
@@ -42,7 +44,21 @@ switch ($_SERVER['REQUEST_METHOD']) {
         }
 
 
-        //ich
+        $return = [];
+        if(isset($_GET["email"])) {
+            $user = User::getUser($_GET["email"]);
+            if (!$user) AnswerHandler::create_response_and_kill_page(false, "user not found", 404);
+                AnswerHandler::create_response_and_kill_page(true, $user);
+            $return['self'] = false;
+        } else {
+            if (!Authentication::is_logged_in())
+                AnswerHandler::create_response_and_kill_page(false, "unauthorized", 401);
+            $user = User::getUser(Authentication::$user_email);
+            $return['self'] = true;
+        }
+
+        $return['profile'] = $user;
+        AnswerHandler::create_response_and_kill_page(true, $return);
         break;
 
     case "DELETE":
