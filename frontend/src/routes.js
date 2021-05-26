5cf6622bb17c8d927f9d181d78d741addda300c5
strukteon <sensentod@gmail.com>
2021-05-26 09:13:30 +0200

    removed id from profile route

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -5,7 +5,7 @@ module.exports = [
         component: () => import('@/pages/home/Home')
     },
     {
-        path: '/profile/:id',
+        path: '/profile',
         name: 'Profile',
         component: () => import('@/pages/profile/Profile')
     },
