0824a23517ec9f4b3d93f978b8d3ce0457255d12
hannik03 <71178510+hannik03@users.noreply.github.com>
2021-05-26 11:11:36 +0200

    Changes by kopsi

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -15,13 +15,13 @@ module.exports = [
         component: () => import('@/pages/search/Search')
     },
     {
-        path: '/imprint',
-        name: 'Imprint',
-        component: () => import('@/pages/imprint/Imprint')
+        path: '/dataprivacy',
+        name: 'Dataprivacy',
+        component: () => import('@/pages/dataprivacy/Dataprivacy')
     },
     {
-        path: '/privacy',
-        name: 'Privacy',
-        component: () => import('@/pages/dataprivacy/Dataprivacy')
+        path: '/settings',
+        name: 'Settings',
+        component: () => import('@/pages/settings/Settings')
     },
 ];
