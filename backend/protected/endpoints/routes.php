b449374737ac43ed54b4860accaaa21c7001b16a
Nicoletta2003-1 <71252015+Nicoletta2003-1@users.noreply.github.com>
2021-05-26 10:24:45 +0200

    fixed /profile

--- backend/protected/endpoints/routes.php
+++ backend/protected/endpoints/routes.php
@@ -13,6 +13,7 @@ Route::add('/users/?',function(){ UserController::index(); });
 Route::add('/user/@me/?',function(){ UserController::show(); });
 Route::add('/user/(.*)/?',function(){ UserController::show(); });
 Route::add('/user/?',function(){ UserController::show(); });
+Route::add('/profile/?',function(){ UserController::show(); });
 //Route::add('/user/?',function(){ UserController::store(); }, 'post');
 Route::add('/user/?',function(){ UserController::update(); }, 'post'); //TODO change to put
 Route::add('/user/ifunsettutor/?',function(){ UserController::ifUnsetTutor(); });
