8135c13118ca18cfac3c5fe7f8714290d9882574
hannik03 <71178510+hannik03@users.noreply.github.com>
2021-06-05 14:26:16 +0200

    fixed aboutus

--- frontend/src/pages/aboutus/Aboutus.vue
+++ frontend/src/pages/aboutus/Aboutus.vue
@@ -1,32 +1,43 @@
 <template>
   <div>
     <img src="@/assets/images/aboutus.jpg" alt="aboutus" class="crop-center">
-    <span class="u1">Über Uns</span>
+    <h1 class="u1">Über Uns</h1>
     <div class="wrapper">
+
       <div class="el1">
-        <span class="u2">Idee</span>
+        <div class="text">
+          <h2 class="u2">Idee</h2>
+          <p>Nachhilfe ist heutzutage an Schulen ein wichtiges Thema, deshalb bieten wir Schüler*innen und Tutoren eine
+            schnelle und effiziente Möglichkeit Nachhilfe innerhalb der Schule zu finden. Das Projekt soll die
+            Organisation und Vermittlung der Nachhilfe verbessern und eine einfach zu bedienende digitale Alternative
+            darstellen. Das Ziel unseres Nachhilfeportals ist, allen Schüler*innen jeglichen Jahrganges eine passende
+            Nachhilfe zu finden. Sie haben die Möglichkeit einen passenden Tutor nach Jahrgang und benötigtem Fach
+            auszuwählen. Unser Nachhilfeportal ist ein schulinternes Projekt, bei welchem sich alle
+            Schüler*innen der HTL Rennweg mit ihrer Schulemail anmelden können. Jeder User besitzt eine Profilseite, auf
+            welcher wichtige Personendaten vorhanden sind: Name, Telefonnummer, E-Mail, persönlicher Stundenplan für die
+            Terminplanung.
+          </p>
+        </div>
         <img class="icons" src="@/assets/images/idee.png" alt="idee">
-        <p class="text">Nachhilfe ist heutzutage an Schulen ein wichtiges Thema, deshalb bieten wir Schüler*innen und Tutoren eine
-          schnelle und effiziente Möglichkeit Nachhilfe innerhalb der Schule zu finden. Das Projekt soll die
-          Organisation und Vermittlung der Nachhilfe verbessern und eine einfach zu bedienende digitale Alternative
-          darstellen. Das Ziel unseres Nachhilfeportals ist, allen Schüler*innen jeglichen Jahrganges eine passende
-          Nachhilfe zu finden. Sie haben die Möglichkeit einen passenden Tutor nach Jahrgang und benötigtem Fach
-          auszuwählen. Unser Nachhilfeportal ist ein schulinternes Projekt, bei welchem sich alle
-          Schüler*innen der HTL Rennweg mit ihrer Schulemail anmelden können. Jeder User besitzt eine Profilseite, auf
-          welcher wichtige Personendaten vorhanden sind: Name, Telefonnummer, E-Mail, persönlicher Stundenplan für die
-          Terminplanung.
-        </p>
       </div><br>
-      <img class="icons1" src="@/assets/images/onedrive.png" alt="onedrive">
-      <div class="text1">
-        <span class="u2">HTL-Rennweg</span>
-        <p>Tutorlink ist ein Projekt der HTL-Rennweg, welches ausschließlich für Schüler*innen dieser Schuleinrichtung
-          verwendet werden kann. Das Projekt soll Schüler*innen unserer Schule helfen und diese beim Lernprozess unterstützen.
-          Mit der Zusammenarbeit der HTL-Rennweg und der Bildungsberatung kann dies ermöglicht werden und in Zukunft an allen Schulen Wiens verwendet werden.</p>
+
+      <div class="el2">
+        <img class="icons" src="@/assets/images/onedrive.png" alt="onedrive">
+        <div class="text">
+          <h2 class="u2">HTL-Rennweg</h2>
+          <p>Tutorlink ist ein Projekt der HTL-Rennweg, welches ausschließlich für Schüler*innen dieser Schuleinrichtung
+            verwendet werden kann. Das Projekt soll Schüler*innen unserer Schule helfen und diese beim Lernprozess unterstützen.
+            Mit der Zusammenarbeit der HTL-Rennweg und der Bildungsberatung kann dies ermöglicht werden und in Zukunft an allen Schulen Wiens verwendet werden.
+          </p>
+        </div>
       </div>
-      <img class="team" src="@/assets/images/group.png" alt="team">
+
     </div>
+
     <div class="people">
+      <div>
+        <img class="team" src="@/assets/images/group.png" alt="team">
+      </div>
       <div class="oben">
         <div>
           <img src="@/assets/images/man.png" alt="man"><br>
@@ -78,7 +89,7 @@ img.crop-center {
   font-weight: 200;
   font-style: initial;
   font-size: 40px;
-  padding-left: 4em;
+  padding-left: 2em;
   letter-spacing: 2.5px;
   color: colors.$fourth;
   margin-top: 20px;
@@ -93,45 +104,41 @@ img.crop-center {
 }
 
 .wrapper {
-  padding-left: 300px;
-  padding-right: 300px;
+  display: flex;
+  flex-direction: column;
+  align-content: space-around;
+  justify-content: space-around;
 }
 
-.el1 {
+.el1{
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+}
 
+.el2{
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+}
+.text{
+  width: 40em;
 }
 
 .icons {
-  width: 15%;
-  float: right;
-  padding-right: 15%;
-  padding-top: 10%;
-}
-
-.icons1{
-  width: 15%;
-  float: left;
-  padding-left: 15%;
-  padding-top: 5%;
+  width: 20em;
+  height: 20em;
 }
 
 p {
   font-size: 1.3em;
 }
 
-.text1{
-  padding-left: 50%;
-}
-
-.text{
-  padding-right: 50%;
-
-}
 
 .team{
   width: 210px;
   padding-top: 50px;
-  padding-left: 43%;
 }
 
 .people{
