b3411d057de2d5f7981b8d1bca456b2213d381e5
hannik03 <71178510+hannik03@users.noreply.github.com>
2021-06-05 21:23:41 +0200

    responsive about us

--- frontend/src/pages/aboutus/Aboutus.vue
+++ frontend/src/pages/aboutus/Aboutus.vue
@@ -5,30 +5,38 @@
     <div class="wrapper">
 
       <div class="el1">
-        <div class="text">
-          <h2 class="u2">Idee</h2>
-          <p>Nachhilfe ist heutzutage an Schulen ein wichtiges Thema, deshalb bieten wir Schüler*innen und Tutoren eine
-            schnelle und effiziente Möglichkeit Nachhilfe innerhalb der Schule zu finden. Das Projekt soll die
-            Organisation und Vermittlung der Nachhilfe verbessern und eine einfach zu bedienende digitale Alternative
-            darstellen. Das Ziel unseres Nachhilfeportals ist, allen Schüler*innen jeglichen Jahrganges eine passende
-            Nachhilfe zu finden. Sie haben die Möglichkeit einen passenden Tutor nach Jahrgang und benötigtem Fach
-            auszuwählen. Unser Nachhilfeportal ist ein schulinternes Projekt, bei welchem sich alle
-            Schüler*innen der HTL Rennweg mit ihrer Schulemail anmelden können. Jeder User besitzt eine Profilseite, auf
-            welcher wichtige Personendaten vorhanden sind: Name, Telefonnummer, E-Mail, persönlicher Stundenplan für die
-            Terminplanung.
-          </p>
+        <div class="half">
+          <div class="text">
+            <h2 class="u2">Idee</h2>
+            <p>Nachhilfe ist heutzutage an Schulen ein wichtiges Thema, deshalb bieten wir Schüler*innen und Tutoren eine
+              schnelle und effiziente Möglichkeit Nachhilfe innerhalb der Schule zu finden. Das Projekt soll die
+              Organisation und Vermittlung der Nachhilfe verbessern und eine einfach zu bedienende digitale Alternative
+              darstellen. Das Ziel unseres Nachhilfeportals ist, allen Schüler*innen jeglichen Jahrganges eine passende
+              Nachhilfe zu finden. Sie haben die Möglichkeit einen passenden Tutor nach Jahrgang und benötigtem Fach
+              auszuwählen. Unser Nachhilfeportal ist ein schulinternes Projekt, bei welchem sich alle
+              Schüler*innen der HTL Rennweg mit ihrer Schulemail anmelden können. Jeder User besitzt eine Profilseite, auf
+              welcher wichtige Personendaten vorhanden sind: Name, Telefonnummer, E-Mail, persönlicher Stundenplan für die
+              Terminplanung.
+            </p>
+          </div>
         </div>
-        <img class="icons" src="@/assets/images/idee.png" alt="idee">
+        <div class="half">
+          <img class="icons" src="@/assets/images/idee.png" alt="idee">
+        </div>
       </div><br>
 
       <div class="el2">
-        <img class="icons" src="@/assets/images/onedrive.png" alt="onedrive">
-        <div class="text">
-          <h2 class="u2">HTL-Rennweg</h2>
-          <p>Tutorlink ist ein Projekt der HTL-Rennweg, welches ausschließlich für Schüler*innen dieser Schuleinrichtung
-            verwendet werden kann. Das Projekt soll Schüler*innen unserer Schule helfen und diese beim Lernprozess unterstützen.
-            Mit der Zusammenarbeit der HTL-Rennweg und der Bildungsberatung kann dies ermöglicht werden und in Zukunft an allen Schulen Wiens verwendet werden.
-          </p>
+        <div class="half">
+          <img class="icons" src="@/assets/images/onedrive.png" alt="onedrive">
+        </div>
+        <div class="half">
+          <div class="text">
+            <h2 class="u2">HTL-Rennweg</h2>
+            <p>Tutorlink ist ein Projekt der HTL-Rennweg, welches ausschließlich für Schüler*innen dieser Schuleinrichtung
+              verwendet werden kann. Das Projekt soll Schüler*innen unserer Schule helfen und diese beim Lernprozess unterstützen.
+              Mit der Zusammenarbeit der HTL-Rennweg und der Bildungsberatung kann dies ermöglicht werden und in Zukunft an allen Schulen Wiens verwendet werden.
+            </p>
+          </div>
         </div>
       </div>
 
@@ -85,83 +93,232 @@ img.crop-center {
   padding-bottom: 10px;
 }
 
-.u1{
-  font-weight: 200;
-  font-style: initial;
-  font-size: 40px;
-  padding-left: 2em;
-  letter-spacing: 2.5px;
-  color: colors.$fourth;
-  margin-top: 20px;
+
+@media (min-width: 950px) {
+  .u1{
+    font-weight: 200;
+    font-style: initial;
+    font-size: 40px;
+    padding-left: 2em;
+    letter-spacing: 2.5px;
+    color: colors.$fourth;
+    margin-top: 20px;
+  }
+
+  .u2 {
+    letter-spacing: 2.5px;
+    color: colors.$fourth;
+    font-style: initial;
+    font-size: 30px;
+
+  }
+  .people img{
+    width:250px;
+  }
+
+  .wrapper {
+    display: flex;
+    flex-direction: column;
+    align-content: space-around;
+    justify-content: space-around;
+  }
+
+  .el1{
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-evenly;
+  }
+
+  .el2{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+  }
+  .text{
+    text-align: justify;
+    width: 40em;
+  }
+
+  .icons {
+    width: 20em;
+    height: 20em;
+  }
+  .half{
+    display: flex;
+    justify-content: center;
+    width: 40%;
+  }
+
+  p {
+    font-size: 1.3em;
+  }
+
+
+  .team{
+    width: 210px;
+    padding-top: 50px;
+  }
+
+  .people{
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    margin-bottom: 20px;
+  }
+
+  .oben,.unten{
+    display: flex;
+    flex-direction: row;
+    align-content: center;
+    text-align: center;
+    padding-bottom: 20px;
+  }
+
+}
+@media (max-width: 949px) {
+  .u1{
+    font-weight: 200;
+    font-style: initial;
+    font-size: 30px;
+    padding-left: 0.5em;
+    letter-spacing: 2.5px;
+    color: colors.$fourth;
+    margin-top: 20px;
+  }
+
+  .u2 {
+    letter-spacing: 2.5px;
+    color: colors.$fourth;
+    font-style: initial;
+    font-size: 1.5em;
+  }
+  .wrapper{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    align-content: center;
+  }
+  p {
+    font-size: 1.2em;
+  }
+  .text{
+    text-align: justify;
+    width: 40em;
+  }
+  .el1{
+    display: flex;
+    flex-direction: column-reverse;
+    justify-content: center;
+    align-items: center;
+    align-content: center;
+  }
+  .el2{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    align-content: center;
+  }
+  .icons {
+    width: 15em;
+    height: 15em;
+  }
+  .people{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    align-content: center;
+  }
+  .people img{
+    width:250px;
+  }
+  .oben{
+    text-align: center;
+  }
+
+  .unten{
+    text-align: center;
+    padding-bottom: 1em;
+  }
+}
+@media (min-width: 660px) {
+  .text{
+    text-align: justify;
+    width: 30em;
+  }
+}
+@media (max-width: 700px) {
+  .u1{
+    font-weight: 200;
+    font-style: initial;
+    font-size: 30px;
+    padding-left: 0.5em;
+    letter-spacing: 2.5px;
+    color: colors.$fourth;
+    margin-top: 20px;
+  }
+
+  .u2 {
+    letter-spacing: 2.5px;
+    color: colors.$fourth;
+    font-style: initial;
+    font-size: 1.5em;
+
+  }
+  .wrapper{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    align-content: center;
+  }
+  p {
+    font-size: 1.2em;
+  }
+  .text{
+    text-align: justify;
+    width: 20em;
+  }
+  .el1{
+    display: flex;
+    flex-direction: column-reverse;
+    justify-content: center;
+    align-items: center;
+    align-content: center;
+  }
+  .el2{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    align-content: center;
+  }
+  .icons {
+    width: 15em;
+    height: 15em;
+  }
+  .people{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    align-content: center;
+  }
+  .people img{
+    width:250px;
+  }
+  .oben{
+    text-align: center;
+  }
+
+  .unten{
+    text-align: center;
+    padding-bottom: 1em;
+  }
 }
 
-.u2 {
-  letter-spacing: 2.5px;
-  color: colors.$fourth;
-  font-style: initial;
-  font-size: 30px;
-
-}
-
-.wrapper {
-  display: flex;
-  flex-direction: column;
-  align-content: space-around;
-  justify-content: space-around;
-}
-
-.el1{
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-around;
-}
-
-.el2{
-  display: flex;
-  flex-direction: row;
-  justify-content: space-around;
-}
-.text{
-  width: 40em;
-}
-
-.icons {
-  width: 20em;
-  height: 20em;
-}
-
-p {
-  font-size: 1.3em;
-}
-
-
-.team{
-  width: 210px;
-  padding-top: 50px;
-}
-
-.people{
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  align-items: center;
-  margin-bottom: 20px;
-}
-
-.oben,.unten{
-  display: flex;
-  flex-direction: row;
-  align-content: center;
-  text-align: center;
-  padding-bottom: 20px;
-}
-
-label{
-  font-family: Dinosaur;
-  font-size: 20px;
-}
-.people img{
-  width:250px;
-}
 </style>
